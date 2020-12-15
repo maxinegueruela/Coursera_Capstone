fd56dba30900e9323f201097472169c98772acbc
maxinegueruela <maxi.fer@live.com.ar>
2020-12-15 20:46:28 -0300

    Add files via upload

---
+++ Predict_for_app.R
@@ -0,0 +1,61 @@
+#Consider to work on working directory
+tidy_unigram <- readRDS("tidy_unigram_25_p.RDS")
+tidy_bigram <- as.data.table(readRDS("tidy_bigram_25_final.RDS"))
+tidy_trigram <- as.data.table(readRDS("tidy_trigram_25_final.RDS"))
+tidy_quadgram <- as.data.table(readRDS("tidy_quadgram_25_final.RDS"))
+
+#Function that cleans and make to lower the input from the user
+clean_intro_text <- function(t){
+  tt <- tolower(t) 
+  tt <- removePunctuation(tt)
+  tt <- removeNumbers(tt)
+  tt <- str_replace_all(tt, "[^[:alnum:]]", " ")
+  tt <- stripWhitespace(tt)
+  return(tt)
+}
+pattern <- "\\w+( \\w+){0,%d}$"
+entrada <- "happy mothers day"
+
+predicted_words <- function(entrada){
+  clean_intro_text(entrada)
+  count <- str_count(entrada, boundary("word"))
+    pattern <- "\\w+( \\w+){0,%d}$"
+    if (count > 0) {
+      predicted <- tidy_quadgram[prefix %in% entrada, last_part]
+      
+      if(length(predicted)>0) return(predicted[1:3])
+      
+      if(length(predicted)==0){
+        entrada_2 <- str_extract(entrada, sprintf(pattern, 1))
+        predicted <- tidy_trigram[prefix %in% entrada_2, last_part]}
+      
+      if(length(predicted)>0) return(predicted[1:3])
+      
+      if(length(predicted)==0){
+        entrada_3 <- str_extract(entrada, sprintf(pattern, 0)) 
+        predicted <- tidy_bigram[prefix %in% entrada_3, last_part]}
+      
+      if(length(predicted)>0) return(predicted[1:3])
+      
+      if(length(predicted)==0){ 
+        entrada_3 <- str_extract(entrada, sprintf(pattern, 0)) 
+        predicted <- tidy_bigram[prefix %in% entrada_3, last_part]} 
+                  
+      if(length(predicted)>0) return(predicted)
+      #in case that predicted has always been 0, then, I select words from the unigrams.
+      #For it, I select the most common unigram and then two random unigrams
+      top_unigram <- tidy_unigram_prob$word[1] #select the most frequent unigram
+      randomWords <- sample(tidy_unigram_prob$word, size = 2) #select 2 random unigrams
+      predicted <- c(top_unigram, randomWords)
+      return(predicted)
+    }
+  
+}
+  
+
+
+
+Tomar de aca 
+https://github.com/eileen98034/Capstone/blob/master/Final/ShinyApplication/predict.R
+https://github.com/mark-blackmore/JHU-Data-Science-Capstone/blob/master/05_Task_Script.md 
+https://github.com/MarcoLeti/DataScience-SwiftKeyCapstoneProject/blob/master/ShinyApp/functions/predictionModel.R 
